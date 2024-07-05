14a84d11d2ed247af3330ebb59cb20af6607375b
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-07-05 09:29:14 +0500

    project completed

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const userModel = require("./models/user");
 
-const userModel = require("./usermodel");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("hello world!");
-});
-
-app.get("/create", async (req, res) => {
-  let createduser = await userModel.create({
-    name: "Kaif Nazeer",
-    email: "kaifnazeer@gmail.com",
-    username: "kaifnazeer",
-  });
-
-  res.send(createduser);
+  res.render("index");
 });
 
 app.get("/read", async (req, res) => {
   let users = await userModel.find();
-  res.send(users);
+  res.render("read", { users });
 });
 
-app.get("/readone", async (req, res) => {
-  let user = await userModel.findOne({ username: "kaifnazeer" });
-  res.send(user);
+app.get("/edit/:userid", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.userid });
+  res.render("edit", { user });
 });
 
-app.get("/update", async (req, res) => {
-  const updateduser = await userModel.findOneAndUpdate(
-    { username: "Kaif" },
-    { name: "Muhammad Kaif Nazee" },
+app.post("/update/:userid", async (req, res) => {
+  let { image, name, email } = req.body;
+
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.userid },
+    { image, name, email },
     { new: true }
   );
-
-  res.send(updateduser);
+  res.redirect("/read");
 });
 
-app.get("/delete", async (req, res) => {
-  let deleteduser = await userModel.findOneAndDelete({
-    username: "kaifnazeer",
+app.get("/delete/:id", async (req, res) => {
+  let users = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/read");
+});
+
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
   });
-  res.send(deleteduser);
+  res.redirect("/read");
 });
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
 
 const userSchema = mongoose.Schema({
-  name: String,
-  username: String,
+  image: String,
   email: String,
+  name: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
